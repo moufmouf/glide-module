44dd4868a360f3690b5708f1fdc5e3cb312fab00
Matthieu Napoli <matthieu@mnapoli.fr>
2015-11-26 23:20:24 +0100

    Simplify the definitions using the latest changes in Assembly

--- src/GlideDefinitionProvider.php
+++ src/GlideDefinitionProvider.php
@@ -2,28 +2,20 @@
 
 namespace GlideModule;
 
-use Assembly\FactoryDefinition;
-use Assembly\InstanceDefinition;
-use Assembly\ParameterDefinition;
-use Assembly\Reference;
-use Interop\Container\Definition\DefinitionProviderInterface;
+use Assembly\ArrayDefinitionProvider;
 
-class GlideDefinitionProvider implements DefinitionProviderInterface
+class GlideDefinitionProvider extends ArrayDefinitionProvider
 {
-    public function getDefinitions()
+    public function getArrayDefinitions()
     {
-        // Options are empty by default
-        $options = new ParameterDefinition('glide.options', []);
-
-        $factory = new InstanceDefinition('glide.factory', 'GlideModule\GlideFactory');
-        $factory->addConstructorArgument(new Reference('glide.options'));
-
-        $server = new FactoryDefinition('glide', new Reference('glide.factory'), 'create');
-
         return [
-            $options,
-            $factory,
-            $server,
+            // Options are empty by default
+            'glide.options' => [],
+
+            'glide.factory' => \Assembly\instance('GlideModule\GlideFactory')
+                ->setConstructorArguments(\Assembly\get('glide.options')),
+
+            'glide' => \Assembly\factory('glide.factory', 'create'),
         ];
     }
 }
