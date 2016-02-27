d02efc9900ce7f98d31b3753414779d07e523f81
Matthieu Napoli <matthieu@mnapoli.fr>
2016-02-27 10:40:39 +0100

    Return PSR-7 response by default

--- src/GlideServiceProvider.php
+++ src/GlideServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace GlideModule;
 
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Stream;
 use Interop\Container\ContainerInterface;
 use Interop\Container\ServiceProvider\ServiceProvider;
+use League\Glide\Responses\PsrResponseFactory;
 use League\Glide\Server;
 use League\Glide\ServerFactory;
 
@@ -19,7 +22,17 @@ class GlideServiceProvider implements ServiceProvider
 
     public static function createGlide(ContainerInterface $container)
     {
-        return ServerFactory::create($container->get('glide.options'));
+        $options = $container->get('glide.options');
+
+        $server = ServerFactory::create($options);
+
+        // We override the response factory to return PSR-7 response by default
+        $responseFactory = new PsrResponseFactory(new Response(), function ($stream) {
+            return new Stream($stream);
+        });
+        $server->setResponseFactory($responseFactory);
+
+        return $server;
     }
 
     public static function getOptions()
